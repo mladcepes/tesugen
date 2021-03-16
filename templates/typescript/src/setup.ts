3ee8a4bd8d410e9cd3397be5f172d23a4b5a0dac
Mladen Radivojevic <mladenr@Mladens-MacBook-Pro.local>
2021-03-16 21:10:33 +0100

    Updating package.json, readme file, updating test file in template, updating bin file, updating test routes

--- templates/typescript/src/setup.ts
+++ templates/typescript/src/setup.ts
@@ -3,9 +3,12 @@ import { testingRoutes } from './routes/testRoutes';
 import { testAgent } from './testAgent';
 
 export class TestController {
-    constructor(
-        public readonly testClientShould: TestClient = new TestClient(testAgent, testingRoutes)
-    ) {}
+    
+    testClientShould: TestClient;
+    
+    constructor() {
+        this.testClientShould = new TestClient(testAgent, testingRoutes);
+    }
 }
 
 export const ctx: TestController = new TestController();
